26153c0dae9303f27f2ebab5372c5389b14ccc88
Camilo Saldías <camilo.saldias@gmail.com>
2017-06-21 14:48:50 -0400

    POST ahora comprueba las credenciales del form Hacemos log de los requests al servidor

--- servidor/ServidorV3.java
+++ servidor/ServidorV3.java
@@ -1,11 +1,14 @@
 package servidor;
 
-import javafx.beans.binding.IntegerBinding;
-
 import java.io.*;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.ServerSocket;
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -37,7 +40,6 @@ class ClientServiceThread extends Thread {
     Socket cs;
     int clientID = -1;
     public String mensajeServidor; //Mensajes entrantes (recibidos) en el servidor
-    public DataOutputStream salidaServidor, salidaCliente; //Flujo de datos de salida
 
     ClientServiceThread(Socket s, int i) {
         cs = s;
@@ -54,19 +56,18 @@ class ClientServiceThread extends Thread {
 
     public void run() {
         try {
-            System.out.println("Cliente en línea");
-
             //Se obtiene el flujo de salida del cliente para enviarle mensajes
             PrintWriter salidaCliente = new PrintWriter(cs.getOutputStream(), true);
 
             //Se obtiene el flujo entrante desde el cliente
             BufferedReader entrada = new BufferedReader(new InputStreamReader(cs.getInputStream(),"UTF-8"));
-            //ArrayList<String> msgList = new ArrayList<>();
 
             //Leemos el header
             String[] header = entrada.readLine().split(" ");
             Map header_data = new HashMap();
-
+            String authorized_user = "username=root&passwd=rdc2017&action=Ingresar";
+            Path access_log = Paths.get("log.txt");
+            String ip = cs.getInetAddress().toString().replace("/", "");
             while ((mensajeServidor = entrada.readLine()) != null) //Mientras haya mensajes desde el cliente
             {
                 String[] line = mensajeServidor.split(": ");
@@ -90,22 +91,35 @@ class ClientServiceThread extends Thread {
                                 break;
                             case "/home_old" :
                                 salidaCliente.println("HTTP/1.1 301 Moved Permanently\n" +
-                                        "Location: http://localhost:1234/\n" +
+                                        "Location: http://127.0.0.1:1234/\n" +
                                         "Server: Java/1.0.0 (MacOS)\n" +
                                         "Content-Type: text/html\n" +
                                         "Connection: close\n\n");
                                 break;
-                            case "/secret" :
-                                salidaCliente.println("HTTP/1.1 403 Forbidden\n" +
-                                        "Server: Java/1.0.0 (MacOS)\n" +
-                                        "Content-Type: text/html\n" +
-                                        "Connection: close\n\n" +
-                                        "<html>\n" +
-                                        "<body>\n" +
-                                        "<h1>Error!</h1>\n" +
-                                        "<p>Usted no tiene permisos para ver esta pagina.</p>" +
-                                        "</body>\n" +
-                                        "</html>\n");
+                            case "/secret":
+                                File check_auth = new File("user_authenticated.txt");
+                                if (check_auth.exists()) {
+                                    salidaCliente.println("HTTP/1.1 200 OK\n" +
+                                            "Server: Java/1.0.0 (MacOS)\n" +
+                                            "Content-Type: text/html\n" +
+                                            "Connection: close\n\n" +
+                                            "<html>\n" +
+                                            "<body>\n" +
+                                            "<h1>Autenticacion completada!</h1>\n" +
+                                            "</body>\n" +
+                                            "</html>\n");
+                                } else {
+                                    salidaCliente.println("HTTP/1.1 403 Forbidden\n" +
+                                            "Server: Java/1.0.0 (MacOS)\n" +
+                                            "Content-Type: text/html\n" +
+                                            "Connection: close\n\n" +
+                                            "<html>\n" +
+                                            "<body>\n" +
+                                            "<h1>Error!</h1>\n" +
+                                            "<p>Usted no tiene permisos para ver esta pagina.</p>" +
+                                            "</body>\n" +
+                                            "</html>\n");
+                                }
                                 break;
                             case "/login" :
                                 salidaCliente.println("HTTP/1.1 200 OK\n" +
@@ -138,34 +152,49 @@ class ClientServiceThread extends Thread {
                             if (utf8Bytes.length >= Integer.parseInt(header_data.get("Content-Length").toString())) break;
                         }
                         System.out.println("Read!");
-                        System.out.println(post_msg);
-
-                        //TODO: comprobar credenciales
+                        if (post_msg.equals(authorized_user)) {
+                            System.out.println("User is valid.");
+                            File auth_mark = new File("user_authenticated.txt");
+                            auth_mark.createNewFile();
+                        }
 
                         switch (header[1]) {
                             case "/secret":
-                                salidaCliente.println("HTTP/1.1 200 OK\n" +
-                                        "Server: Java/1.0.0 (MacOS)\n" +
-                                        "Content-Type: text/html\n" +
-                                        "Connection: close\n\n" +
-                                        "<html>\n" +
-                                        "<body>\n" +
-                                        "<h1>Error!</h1>\n" +
-                                        "<p>Usted no tiene permisos para ver esta pagina.</p>" +
-                                        "</body>\n" +
-                                        "</html>\n");
+                                File check_auth = new File("user_authenticated.txt");
+                                if (check_auth.exists()) {
+                                    salidaCliente.println("HTTP/1.1 200 OK\n" +
+                                            "Server: Java/1.0.0 (MacOS)\n" +
+                                            "Content-Type: text/html\n" +
+                                            "Connection: close\n\n" +
+                                            "<html>\n" +
+                                            "<body>\n" +
+                                            "<h1>Autenticacion completada!</h1>\n" +
+                                            "</body>\n" +
+                                            "</html>\n");
+                                } else {
+                                    salidaCliente.println("HTTP/1.1 403 Forbidden\n" +
+                                            "Server: Java/1.0.0 (MacOS)\n" +
+                                            "Content-Type: text/html\n" +
+                                            "Connection: close\n\n" +
+                                            "<html>\n" +
+                                            "<body>\n" +
+                                            "<h1>Error!</h1>\n" +
+                                            "<p>Usted no tiene permisos para ver esta pagina.</p>" +
+                                            "</body>\n" +
+                                            "</html>\n");
+                                }
                                 break;
                         }
                     }
 
-                    //TODO: hacer el log!
                     entrada.close();
                     break;
                 }
 
             }
 
-            System.out.println("Cliente Desconectado.");
+            List<String> lines = Arrays.asList(ip+" "+header[0]+" "+header[1]+" "+getServerTime());
+            Files.write(access_log, lines, Charset.forName("UTF-8"), StandardOpenOption.APPEND);
 
         }
         catch (Exception e)
